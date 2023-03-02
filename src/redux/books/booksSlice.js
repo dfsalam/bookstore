822fbd4ee3aef35df5514f73c0edc947455c370d
dfsalam <dfsalam@gmail.com>
2023-03-01 22:03:51 -0500

    Define a slice of state for books that: -Includes an array of books (initial state: empty array). -Includes a reducer that adds a book. -Includes a reducer that removes a book.

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  books: [],
+}
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers:{
+    addBook:(state)=> {
+      const book = {item_id:'item1', title:'Python Data Analytics', author:'Fabio Nelli', category:'Python'}
+      state.books.push(book);
+    },
+    removeBook:(state, action) => {
+      const itemId = action.payload;
+      state.books = state.cartItems.filter((item)=>
+        item.id !== itemId);
+    },    
+  }
+})
