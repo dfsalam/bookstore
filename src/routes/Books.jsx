4321c4ab63022501f284feb82721c6d8eee637f4
dfsalam <dfsalam@gmail.com>
2023-03-03 22:28:54 -0500

    Change book loading to the Books tab

--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import BookCntr from '../components/BookCntr';
 import Form from '../components/Form';
+import { getBooksItems } from '../redux/books/booksSlice';
 
 export default function Books() {
+  const {
+    isLoading,
+  } = useSelector((state) => state.books);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooksItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h3>Is loading .....</h3>
+      </div>
+    );
+  }
   return (
     <div>
       <BookCntr />
